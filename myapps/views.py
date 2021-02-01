aff6e33d50b4d3db5e6719cb16f842cdfe1043e8
lixin <lixin@lixindeMacBook-Pro.local>
2021-02-01 22:47:48 +0800

    修改了管理界面

--- myapps/views.py
+++ myapps/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-
 # Create your views here.
 
 
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Question, Choice
+from django.template import loader
+from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
 
 def detail(request, question_id):
@@ -11,16 +16,27 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'myapps/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
-
-
-from .models import Question
-from django.template import loader
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'myapps/details.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('myapps:results', args=(question.id,)))
 
 
 def index(request):
@@ -40,10 +56,6 @@ def index(request):
     return render(request, 'myapps/index.html', context)
 
 
-from django.http import Http404
-from django.shortcuts import render, get_object_or_404
-
-
 # def details(request, question_id):
 #     try:
 #         question = Question.objects.get(pk=question_id)
@@ -53,5 +65,32 @@ from django.shortcuts import render, get_object_or_404
 
 def details(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    error_message = 'hello'
+    error_message = 'hello1'
     return render(request, 'myapps/details.html', {'question': question, 'error_message': error_message})
+
+
+class IndexView(generic.ListView):
+    template_name = 'myapps/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'myapps/details.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'myapps/results.html'
